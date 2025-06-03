67e41975ad2ece70a6723be4408d8c64be24418a
Shostok <ivanchechutkov@gmail.com>
2025-06-03 16:18:12 +0300

    Added Api json

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,23 @@
+import axios from 'axios';
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const API_URL = () => {
+    axios
+      .get('https://jsonplaceholder.typicode.com/users')
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    API_URL();
+  }, []);
+
   return <></>;
 }
 
