be6c423d6ef35bd30bf8c52921734976c3ddd912
Shostok <ivanchechutkov@gmail.com>
2025-06-03 17:18:18 +0300

    Created axios API query

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,12 @@
-import axios from 'axios';
-import { useEffect } from 'react';
 import './App.css';
+import Users from './components/Users';
 
 function App() {
-  const API_URL = () => {
-    axios
-      .get('https://jsonplaceholder.typicode.com/users')
-      .then(response => {
-        console.log(response.data);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  };
-
-  useEffect(() => {
-    API_URL();
-  }, []);
-
-  return <></>;
+  return (
+    <>
+      <Users />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Users.jsx
@@ -0,0 +1,46 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import './user.css';
+
+function Users() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get('https://jsonplaceholder.typicode.com/users')
+      .then(response => {
+        setUsers(response.data);
+        setLoading(false);
+      })
+      .catch(error => {
+        setError(error.message);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return <div className="loading">Loading...</div>;
+  if (error) return <div className="error">Error: {error}</div>;
+
+  return (
+    <div className="App">
+      <h1>Users Information</h1>
+      <div className="users-container">
+        {users.map(user => (
+          <div key={user.id} className="user-card">
+            <h2>{user.name}</h2>
+            <p>
+              <strong>Username:</strong> {user.username}
+            </p>
+            <p>
+              <strong>Email:</strong> {user.email}
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Users;
