8dffe116fddde28f790824853c88e82ccd57ff85
Shostok <ivanchechutkov@gmail.com>
2025-06-10 15:14:15 +0300

    Added post page

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router';
 import { Layout } from '../Layout/Layout';
 import { NotFound } from '../NotFound/NotFound';
+import { PostDetails } from '../PostDetails/PostDetail';
 import { Posts } from '../Posts/Posts';
 import { UserDetails } from '../UserDetails/UserDetails';
 import { Users } from '../Users/Users';
@@ -14,6 +15,7 @@ export function App() {
           <Route path="/" element={<Layout />}>
             <Route index element={<Users />} />
             <Route path="/posts" element={<Posts />} />
+            <Route path="/posts/:id" element={<PostDetails />} />
             <Route path="/users/:id" element={<UserDetails />} />
             <Route path="*" element={<NotFound />} />
           </Route>
---
+++ src/components/PostDetails/PostDetail.jsx
@@ -0,0 +1,75 @@
+import { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router';
+import { getPost } from '../../api/postApi';
+import { Button } from '../Button/Button';
+import { Error } from '../Error/Error';
+import { Loader } from '../Loader/Loader';
+import styles from './PostDetail.module.css';
+
+export function PostDetails() {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [post, setPost] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const handleBackClick = () => {
+    navigate('/');
+  };
+
+  useEffect(() => {
+    if (id) {
+      getPost(id)
+        .then(response => {
+          setPost(response.data);
+        })
+        .catch(err => {
+          setError(err.message || 'Failed to fetch user data');
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    }
+  }, [id]);
+
+  if (loading) {
+    return <Loader text="Loading post data..." />;
+  }
+
+  if (error) {
+    return (
+      <Error
+        error={error}
+        buttonText="Return to Posts List"
+        onClick={handleBackClick}
+        hasButton
+      />
+    );
+  }
+
+  if (!post) {
+    return (
+      <Error
+        error="Post not found"
+        buttonText="Return to Posts List"
+        onClick={handleBackClick}
+        hasButton
+      />
+    );
+  }
+
+  return (
+    <>
+      <h1>Posts Details</h1>
+      <div className={styles.postsDetails}>
+        <h2>
+          Title: <br /> {post.title}
+        </h2>
+
+        <p>{post.body}</p>
+
+        <Button onClick={handleBackClick}>Back to Users List</Button>
+      </div>
+    </>
+  );
+}
